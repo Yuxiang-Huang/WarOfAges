66eac9aefdb5d8ee29080a718b65d981be06d43b
Yuxiang Huang <yhuang30@stuy.edu>
2023-03-21 21:51:01 -0400

    Add attack code for speed troop

--- WarOfAges/Assets/Scripts/Yuxiang/Unit/Troop/Speed.cs
+++ WarOfAges/Assets/Scripts/Yuxiang/Unit/Troop/Speed.cs
@@ -6,6 +6,35 @@ using UnityEngine;
 
 public class Speed : Troop
 {
+    public override void attack()
+    {
+        SortedDictionary<float, Tile> targets = new SortedDictionary<float, Tile>();
+
+        //check all surrounding tiles
+        foreach (Tile curTile in tile.neighbors)
+        {
+            //if can see this tile and there is enemy unit on it
+            if (!curTile.dark.activeSelf && curTile.unit != null && curTile.unit.ownerID != ownerID)
+            {
+                //attack order depending on dot product
+                targets.TryAdd(Vector2.Dot(direction,
+                    TileManager.instance.getWorldPosition(curTile) - TileManager.instance.getWorldPosition(tile)),
+                    curTile);
+            }
+        }
+
+        //don't attack already dead troop
+        while (targets.Count > 0 && targets.Values.Last().unit.health <= 0)
+        {
+            targets.Remove(targets.Keys.Last());
+        }
+
+        if (targets.Count > 0)
+        {
+            targets.Values.Last().unit.PV.RPC(nameof(takeDamage), RpcTarget.AllViaServer, damage);
+        }
+    }
+
     public override void move()
     {
         base.move();
