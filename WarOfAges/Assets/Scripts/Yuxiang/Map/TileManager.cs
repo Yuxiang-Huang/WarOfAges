092080c26b9bf83fa578514560002f2a00ea1c43
Yuxiang Huang <yhuang30@stuy.edu>
2023-05-16 09:54:27 -0400

    Start hex map

--- WarOfAges/Assets/Scripts/Yuxiang/Map/TileManager.cs
+++ WarOfAges/Assets/Scripts/Yuxiang/Map/TileManager.cs
@@ -30,100 +30,6 @@ public class TileManager : MonoBehaviourPunCallbacks
     public Dictionary<Vector2Int, int> neighborIndexOddRow;
     public Dictionary<Vector2Int, int> neighborIndexEvenRow;
 
-    #region Perlin Noise
-
-    // Perlin Noise Bits
-    public float fade(float x)
-    {
-        return x * x * x * (x * (6 * x - 15) + 10);
-        // x^3 ( x (6x - 15) + 10) = x^3 ( 6x^2 - 15x + 10) = 6x^5 - 15x^4 + 10x^3
-    }
-    public float linInterp(float a, float b, float c)
-    {
-        return b + a * (c - b);
-        // b + ac - ab = ac - b(a-1)
-    }
-    public float gradient(int hash, float x, float y, float z)
-    {
-        int h = hash & 15;  // 4 bits
-        float u = h < 8 ? x : y;   // u is x if h < 8; and y otherwise
-        float v = h < 4 ? y : h == 12 || h == 14 ? x : z;  // if h is less than 4, v is y. if h is 12 or 14, v is x. Else, v is z;
-
-        // if the lowest bit of h is 0, u is positive, else u is negative
-        // if the second bit digit of h is 0, v is positive. Else v is negative;
-        if ((h & 1) == 1) { u *= -1; }
-        if ((h & 2) == 1) { v *= -1; }
-
-        return u + v;
-    }
-
-    public int[] randomVals = { 151,160,137,91,90,15,
-           131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
-           190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
-          88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
-           77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
-           102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
-           135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
-           5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
-           223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
-           129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
-           251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
-           49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
-           138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180
-       };
-
-    public int[] usedVals = new int[512];
-
-    public float pNoise(float x, float y, float z)
-    {
-
-        // First we find a unit cube that encompasses the point
-        int cubeX = (int)x & 255; // This reduces the number of the cube to 8 bits
-        int cubeY = (int)y & 255; // This reduces the number of the cube to 8 bits
-        int cubeZ = (int)z & 255; // This reduces the number of the cube to 8 bits
-
-        float pointX = x - cubeX; // The point on that cube
-        float pointY = y - cubeY;
-        float pointZ = z - cubeZ;
-
-        // Creating a fade for each point
-        float a = fade(pointX);
-        float b = fade(pointY);
-        float c = fade(pointZ);
-
-        // Creating additional points for vectors
-        int A = usedVals[cubeX] + cubeY,
-            A0 = usedVals[A] + cubeZ,
-            A1 = usedVals[A + 1] + cubeZ,
-            B = usedVals[cubeX + 1] + cubeY,
-            B0 = usedVals[B] + cubeZ,
-            B1 = usedVals[B + 1] + cubeZ;
-
-        // Take these points, and form the vectors, and interpolate
-        return linInterp(c,
-                 linInterp(b,
-                       linInterp(a,
-                             gradient(usedVals[A0], pointX, pointY, pointZ),
-                             gradient(usedVals[B0], pointX - 1, pointY, pointZ)
-                               ),
-                       linInterp(a,
-                             gradient(usedVals[A1], pointX, pointY - 1, pointZ),
-                             gradient(usedVals[B1], pointX - 1, pointY - 1, pointZ)
-                               )
-                 ),
-                 linInterp(b,
-                       linInterp(a,
-                             gradient(usedVals[A0 + 1], pointX, pointY, pointZ - 1),
-                             gradient(usedVals[B0 + 1], pointX - 1, pointY, pointZ - 1)
-                               ),
-                       linInterp(a,
-                              gradient(usedVals[A1 + 1], pointX, pointY - 1, pointZ - 1),
-                              gradient(usedVals[B1 + 1], pointX - 1, pointY - 1, pointZ - 1))));
-
-    }
-
-    #endregion
-
     void Awake()
     {
         instance = this;
@@ -149,21 +55,25 @@ public class TileManager : MonoBehaviourPunCallbacks
         }
 
         //check borders in prefab
-        neighborIndexOddRow = new Dictionary<Vector2Int, int>();
-        neighborIndexOddRow.Add(new Vector2Int(-2, 0), 3);
-        neighborIndexOddRow.Add(new Vector2Int(2, 0), 5);
-        neighborIndexOddRow.Add(new Vector2Int(-1, 0), 1);
-        neighborIndexOddRow.Add(new Vector2Int(1, 0), 0);
-        neighborIndexOddRow.Add(new Vector2Int(-1, 1), 2);
-        neighborIndexOddRow.Add(new Vector2Int(1, 1), 4);
+        neighborIndexOddRow = new Dictionary<Vector2Int, int>
+        {
+            { new Vector2Int(-2, 0), 3 },
+            { new Vector2Int(2, 0), 5 },
+            { new Vector2Int(-1, 0), 1 },
+            { new Vector2Int(1, 0), 0 },
+            { new Vector2Int(-1, 1), 2 },
+            { new Vector2Int(1, 1), 4 }
+        };
 
-        neighborIndexEvenRow = new Dictionary<Vector2Int, int>();
-        neighborIndexEvenRow.Add(new Vector2Int(-2, 0), 3);
-        neighborIndexEvenRow.Add(new Vector2Int(2, 0), 5);
-        neighborIndexEvenRow.Add(new Vector2Int(-1, 0), 2);
-        neighborIndexEvenRow.Add(new Vector2Int(1, 0), 4);
-        neighborIndexEvenRow.Add(new Vector2Int(-1, -1), 1);
-        neighborIndexEvenRow.Add(new Vector2Int(1, -1), 0);
+        neighborIndexEvenRow = new Dictionary<Vector2Int, int>
+        {
+            { new Vector2Int(-2, 0), 3 },
+            { new Vector2Int(2, 0), 5 },
+            { new Vector2Int(-1, 0), 2 },
+            { new Vector2Int(1, 0), 4 },
+            { new Vector2Int(-1, -1), 1 },
+            { new Vector2Int(1, -1), 0 }
+        };
     }
 
     [PunRPC]
@@ -181,64 +91,15 @@ public class TileManager : MonoBehaviourPunCallbacks
 
     public void makeGrid()
     {
-        //store type of tiles using bit
-        StringBuilder instruction = new StringBuilder();
+        int rows = Config.mapRadius * 4;
+        int cols = Config.mapRadius * 4;
 
-        // Random array of  512 values that are [0, 255]
-        for (int i = 0; i < 256; i++)
-        {
-            usedVals[i] = randomVals[i];
-            usedVals[256 + i] = randomVals[i];
-        }
-        float ranElem = Random.Range(0, 200);
-
-        int rows = Config.mapSize.x;
-        int cols = Config.mapSize.y;
+        string[,] instructionGrid = new string[rows, cols];
 
         //decide map
         if ((string)PhotonNetwork.CurrentRoom.CustomProperties["Mode"] == "Water")
         {
-            for (int row = 0; row < rows; row++)
-            {
-                for (int col = 0; col < cols; col++)
-                {
-                    ////border by water
-                    //if (row <= 4 || row >= rows - 5)
-                    //{
-                    //    instruction.Append(1);
-                    //}
-                    //else if (row % 2 == 0 && col == 1)
-                    //{
-                    //    instruction.Append(1);
-                    //}
-                    //else if (row % 2 == 1 && col <= 1)
-                    //{
-                    //    instruction.Append(1);
-                    //}
-                    //else if (row % 2 == 0 && col == cols - 1)
-                    //{
-                    //    instruction.Append(1);
-                    //}
-                    //else if (row % 2 == 1 && col >= cols - 2)
-                    //{
-                    //    instruction.Append(1);
-                    //}
-                    //else
-                    //{
-                        float noiseNum = pNoise(17f * col / cols + ranElem, 17f * row / rows + ranElem, 0) + 0.1f;
-
-                        //assign type
-                        if (noiseNum >= 0)
-                        {
-                            instruction.Append(0);
-                        }
-                        else
-                        {
-                            instruction.Append(1);
-                        }
-                    //}
-                }
-            }
+            
         }
         else
         {
@@ -246,8 +107,19 @@ public class TileManager : MonoBehaviourPunCallbacks
             {
                 for (int col = 0; col < cols; col++)
                 {
-                    instruction.Append(0);
+                    instructionGrid[row, col] = "0";
                 }
+            }
+        }
+
+        //store type of tiles using bit
+        StringBuilder instruction = new();
+
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                instruction.Append(instructionGrid[row, col]);
             }
         }
 
@@ -281,24 +153,27 @@ public class TileManager : MonoBehaviourPunCallbacks
 
                     Vector3 pos = new Vector3(xPos, yPos, 0);
 
-                    //instantiate
-                    if (instruction[count] == '0')
+                    if (instruction[count] != '0')
                     {
-                        tiles[i, j] = Instantiate(landTilePrefab, pos, Quaternion.identity).GetComponent<Tile>();
-                        tiles[i, j].terrain = "land";
+                        //instantiate
+                        if (instruction[count] == '1')
+                        {
+                            tiles[i, j] = Instantiate(landTilePrefab, pos, Quaternion.identity).GetComponent<Tile>();
+                            tiles[i, j].terrain = "land";
 
-                        totalLandTiles++;
+                            totalLandTiles++;
+                        }
+                        else if (instruction[count] == '2')
+                        {
+                            tiles[i, j] = Instantiate(waterTilePrefab, pos, Quaternion.identity).GetComponent<Tile>();
+                            tiles[i, j].terrain = "water";
+                        }
+
+                        //set tile stats
+                        tiles[i, j].transform.SetParent(parent.transform);
+
+                        tiles[i, j].GetComponent<Tile>().pos = new Vector2Int(i, j);
                     }
-                    else
-                    {
-                        tiles[i, j] = Instantiate(waterTilePrefab, pos, Quaternion.identity).GetComponent<Tile>();
-                        tiles[i, j].terrain = "water";
-                    }
-
-                    //set tile stats
-                    tiles[i, j].transform.SetParent(parent.transform);
-
-                    tiles[i, j].GetComponent<Tile>().pos = new Vector2Int(i, j);
                 }
 
                 count++;
@@ -310,8 +185,8 @@ public class TileManager : MonoBehaviourPunCallbacks
         {
             for (int col = 0; col < tiles.GetLength(1); col++)
             {
-                //skip bottom row
-                if (!(row % 2 == 0 && col == 0))
+                //skip null
+                if (tiles[row, col] != null)
                 {
                     List<Tile> neighbors = tiles[row, col].neighbors;
 
@@ -376,7 +251,7 @@ public class TileManager : MonoBehaviourPunCallbacks
                         }
                     }
 
-                    //remove null due to skip bottom row
+                    //remove null tiles
                     for (int i = neighbors.Count - 1; i >= 0; i--)
                     {
                         if (neighbors[i] == null)
@@ -393,8 +268,8 @@ public class TileManager : MonoBehaviourPunCallbacks
         {
             for (int col = 0; col < tiles.GetLength(1); col++)
             {
-                //skip bottom row
-                if (!(row % 2 == 0 && col == 0))
+                //skip null
+                if (tiles[row, col] != null)
                 {
                     tiles[row, col].neighbors2 = findNeighbors2(tiles[row, col]);
                 }
--- WarOfAges/Assets/Scripts/Yuxiang/Tool/GlobalSuppressions.cs
+++ WarOfAges/Assets/Scripts/Yuxiang/Tool/GlobalSuppressions.cs
@@ -1,2 +1,5 @@
 ﻿//no warning for naming functions with camelcase
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("IDE", "IDE1006")]
+
+//no warning for instantiation simplification
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("IDE", "IDE0090")]
