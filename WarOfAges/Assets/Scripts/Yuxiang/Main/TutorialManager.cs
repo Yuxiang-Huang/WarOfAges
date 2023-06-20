53bd24a5457075331db5530e65da6ad3789604b4
GreenCat <tedd0328@gmail.com>
2023-06-20 01:42:16 -0400

    New TutorialText + All Arrows and Filters
    All the tutorial advances are coded in.

--- WarOfAges/Assets/Scripts/Yuxiang/Main/TutorialManager.cs
+++ WarOfAges/Assets/Scripts/Yuxiang/Main/TutorialManager.cs
@@ -64,25 +64,7 @@ public class TutorialManager : MonoBehaviour
 
     void Update()
     {
-        // Welcome! Begin your game by placing your <b>base</b> on a visible green <b>tile</b> on the map. Each tile can either be water (blue) or land (green), but only land tiles can generate <b>income</b>.
         if (index == 0)
-        {
-            // advance when player spawned main base
-            if (PlayerController.instance.mainBase != null)
-            {
-                advance();
-            }
-        }
-
-        // The highlighted tiles are your <b>territory</b>. You will learn how to expand your territory later.
-        // <br><br>Click anywhere to continue.
-
-        // Here is the player info tab, where you can view info about each player.For now, know that the hexagon symbol shows how many tiles are your territory and the coin symbol shows how many<b>gold</b> you have.
-        // <br><br>Click anywhere to continue.
-
-        // Here you can also see the amount of Gold you have. The second number shows the income you will get after this <b>turn</b>.
-        // <br><br>Click anywhere to continue.
-        if (index == 1 || index == 2 || index == 3)
         {
             // advance when clicked
             if (Input.GetMouseButtonDown(0))
@@ -91,8 +73,25 @@ public class TutorialManager : MonoBehaviour
             }
         }
 
-        // Now end your turn by clicking the End Turn button. Notice that your gold increases.
-        if (index == 4)
+        if (index == 1)
+        {
+            // advance when base is placed
+            if (PlayerController.instance.mainBase != null)
+            {
+                advance();
+            }
+        }
+
+        if (index == 2 || index == 3 || index == 4)
+        {
+            // advance when clicked
+            if (Input.GetMouseButtonDown(0))
+            {
+                advance();
+            }
+        }
+
+        if (index == 5)
         {
             // advance when it is the second turn
             if (UIManager.instance.getTurnNum() > 1)
@@ -101,8 +100,16 @@ public class TutorialManager : MonoBehaviour
             }
         }
 
-        // Here is the shop. You can view info about each <b>unit</b> by hovering or holding on its image. Let's learn how to spawn a unit now. First, you select a unit in the shop. Most units are either a <b>troop</b> or <b>building</b>, but there are <i>exceptions</i>…
-        if (index == 5)
+        if (index == 6 || index == 7)
+        {   
+            // advance when clicked
+            if (Input.GetMouseButtonDown(0))
+            {
+                advance();
+            }
+        }
+
+        if (index == 8)
         {
             // advance when a spawnButton is selected
             if (PlayerController.instance.toSpawnUnit != null)
@@ -111,8 +118,7 @@ public class TutorialManager : MonoBehaviour
             }
         }
 
-        // This unit is a troop. You can view its info in the <b>unit info bar</b> on the right. You can spawn troops only on territory adjacent to a building. Your base is a building. Let's spawn your first troop!
-        if (index == 6)
+        if (index == 9)
         {
             // advance when a unit is spawned
             if (PlayerController.instance.spawnList.Count != 0)
@@ -121,8 +127,7 @@ public class TutorialManager : MonoBehaviour
             }
         }
 
-        // Let's deselect the unit in the shop by clicking anywhere that is not a spawnable tile.
-        if (index == 7)
+        if (index == 10)
         {
             // advance when the spawnButton is deselected
             if (PlayerController.instance.toSpawnUnit == null)
@@ -131,20 +136,8 @@ public class TutorialManager : MonoBehaviour
             }
         }
 
-        // Now let's click on the unit you just put on the spawn list.
-        if (index == 8)
-        {
-            // advance when a spawninfo is selected
-            if (PlayerController.instance.spawnInfoSelected != null)
-            {
-                advance();
-            }
-        }
-
-        // You can see its info in the unit info bar. You can choose to despawn it and you will get all the gold used to spawn this unit back.
-        // <br><br>Click anywhere to continue.
-        if (index == 9)
-        {
+        if (index == 11)
+        {   
             // advance when clicked
             if (Input.GetMouseButtonDown(0))
             {
@@ -152,18 +145,7 @@ public class TutorialManager : MonoBehaviour
             }
         }
 
-        // Put a unit in the spawn list.
-        if (index == 10)
-        {
-            // advance when a unit is put on spawn list
-            if (PlayerController.instance.spawnList.Count != 0)
-            {
-                advance();
-            }
-        }
-
-        // Units will be spawned after the turn ends. Let's end your turn by clicking the End Turn button.
-        if (index == 11)
+        if (index == 12)
         {
             // advance when it is the third turn
             if (UIManager.instance.getTurnNum() > 2)
@@ -172,10 +154,8 @@ public class TutorialManager : MonoBehaviour
             }
         }
 
-        // Remember the player info tab? The soldier icon shows how many troops you have and the house icon shows how many buliding you have.
-        // <br><br>Click anywhere to continue.
-        if (index == 12)
-        {
+        if (index == 13)
+        {   
             // advance when clicked
             if (Input.GetMouseButtonDown(0))
             {
@@ -183,17 +163,6 @@ public class TutorialManager : MonoBehaviour
             }
         }
 
-        // Let's select the troop you just spawned.
-        if (index == 13)
-        {
-            // advance when a troop is selected
-            if (PlayerController.instance.unitSelected != null)
-            {
-                advance();
-            }
-        }
-
-        // Again, you can see its info in the unit info bar. To move your troop now, simply click on any tile you want it to move to.
         if (index == 14)
         {
             // advance when troop has an arrow
@@ -205,8 +174,16 @@ public class TutorialManager : MonoBehaviour
             }
         }
 
-        // The red arrow shows the tile this troop will move to after the turn ends. End the turn by clicking the End Turn Button or the turn will automatically end after the time runs out.
         if (index == 15)
+        {   
+            // advance when clicked
+            if (Input.GetMouseButtonDown(0))
+            {
+                advance();
+            }
+        }
+
+        if (index == 16)
         {
             // advance when it is the fourth turn
             if (UIManager.instance.getTurnNum() > 3)
@@ -215,8 +192,7 @@ public class TutorialManager : MonoBehaviour
             }
         }
 
-        // The troop will continue to move to its destination tile, which you set last turn. To prevent a troop from moving next turn, you can simply set its destination tile to the tile it is on.<br><br>Click anywhere to continue.
-        if (index == 16)
+        if (index > 16 && index <= 20)
         {
             // advance when clicked
             if (Input.GetMouseButtonDown(0))
@@ -225,7 +201,25 @@ public class TutorialManager : MonoBehaviour
             }
         }
 
-        if (index > 16)
+        if (index == 21)
+        {
+            // advance when it is the fourth turn
+            if (UIManager.instance.getTurnNum() > 3)
+            {
+                advance();
+            }
+        }
+
+        if (index == 22)
+        {
+            // advance when it is the fourth turn
+            if (UIManager.instance.getTurnNum() > 3)
+            {
+                advance();
+            }
+        }
+
+        if (index > 22)
         {
             // advance when clicked
             if (Input.GetMouseButtonDown(0))
